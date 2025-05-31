2bf24d07bcd8a17939b528753107d990451a9495
phnguynmkhoi <pnmk0811@gmail.com>
2025-05-31 13:17:48 +0700

    add Dockerfile for backend

---
+++ src/backend/db_config.py
@@ -0,0 +1,26 @@
+DB_USER = "root"
+DB_PASSWORD = "admin"
+DB_HOST = "mysql"
+DB_PORT = "3306"
+DB_NAME = "movie_recommendation"
+
+MONGO_HOST = "mongo"
+MONGO_PORT = "27017"
+MONGO_USERNAME = "root"
+MONGO_PASSWORD = "example"
+MONGO_URI = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}"
+MONGO_DB_NAME = "movie_db"
+
+ES_SCHEMA = "http"
+ES_HOST = "elasticsearch"
+ES_PORT = "9200"
+ES_USERNAME = "elastic"
+ES_PASSWORD = "changeme"
+ES_URL = f"{ES_SCHEMA}://{ES_HOST}:{ES_PORT}"
+
+CHROMA_DB_PATH = "chroma/" 
+
+REDIS_HOST = "redis"
+REDIS_PORT = "6379"
+REDIS_DB = 0
+REDIS_PASSWORD = None
